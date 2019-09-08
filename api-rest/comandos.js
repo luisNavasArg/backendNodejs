83094c8277cac40a3142d53cf70329cbc2157608
LuisNavasArg <lpipnavas@gmail.com>
2019-09-08 14:59:34 -0300

    Instalamos body parser y nodemos

--- api-rest/comandos.js
+++ api-rest/comandos.js
@@ -10,6 +10,14 @@ let comandos=[
 	{
 	comando: "node index.js",
 	descripcion:"Para iniciar el servidor"
+	},
+	{
+	comando:"npm i -S body-parser",
+	descripcion:"Para instalar modulo encargado de parsear todo con las peticiones http"
+	},
+	{
+		comando:"npm i -D nodemon",
+		descripcion:"Para que no tengamos que reiniciar el servidor cuando hagamos un cambio"
 	}
 
 	
--- api-rest/index.js
+++ api-rest/index.js
@@ -1,9 +1,14 @@
 'use strict' //para funcionalidades ecmas6
 //node no soporta import
 const express = require('express')
+//importamos body-parser
+const bodyParser = require('body-parser')
+
 
 const app = express()
-
+//añadimos el metodo body parser
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
 const port = 3000
 
 app.listen(port, ()=>{
