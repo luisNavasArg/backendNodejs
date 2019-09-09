a5d8e3271f81b9bfbe2073fb24bf1dff3adface3
LuisNavasArg <lpipnavas@gmail.com>
2019-09-09 00:14:45 -0300

    verbos get post put y delete

--- api-rest/index.js
+++ api-rest/index.js
@@ -6,11 +6,35 @@ const bodyParser = require('body-parser')
 
 
 const app = express()
+// variable para el puerto 
+const port = process.env.PORT || 3000
 //añadimos el metodo body parser
 app.use(bodyParser.urlencoded({extended:false}))
+// instalar json formster
 app.use(bodyParser.json())
-const port = 3000
+//uso del metodo get com parametros
+// app.get('/hola/:nombre',(req,res)=>{
+// 	res.send({mensaje:`Estas en el servidor ${req.params.nombre}`});
+// })
+//verbos para la apirest full
+//uso del metodo get com parametros
+app.get('/api/productos',(req,res)=>{
+	res.status(200).send({productos:[]})
+})
 
+app.get('/api/productos:productoId',(req,res)=>{
+		
+})
+app.post('/api/productos',(req,res)=>{
+	console.log(req.body)
+	res.status(200).send({mensaje:'producto recibido'})	
+})
+app.put('/api/productos:productoId',(req,res)=>{
+		
+})
+app.delete('/api/productos:productoId',(req,res)=>{
+		
+})
 app.listen(port, ()=>{
 	console.log(`corriendo en el puerto ${port}`)
 })
